dff4d91b7ffc0b4d478aae51e31673d6a3bd2f19
greyes97 <greyesl1@miumg.edu.gt>
2021-04-25 22:20:39 -0600

    circle_sort files added

---
+++ src/main/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/parcial2/circle_sort/CircleSortApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.parcial2.circle_sort;
+
+import gt.edu.umg.ingenieria.sistemas.ingenieria_software.parcial2.circle_sort.service.CicleSort;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleSortApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/test/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/parcial2/circle_sort/CircleSortApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.parcial2.circle_sort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleSortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
